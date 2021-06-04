888d52dceee7a90a34f53051f79892a78641328f
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2021-06-04 22:50:10 +0500

    Create Recursive_Karatsuba_mult.py

---
+++ Recursive_Karatsuba_mult.py
@@ -0,0 +1,44 @@
+""" Recursive Approach """
+def zeroInsert(numberString, zeros, left = True):
+    """Return the string with zeros added to the left or right."""
+    for i in range(zeros):
+        if left:
+            numberString = '0' + numberString
+        else:
+            numberString = numberString + '0'
+    return numberString 
+
+def karatsubaMultiplication(x ,y):
+    """Multiply two integers using Karatsuba's algorithm."""
+    #convert to strings for easy access to digits
+    x = str(x)
+    y = str(y)
+    #base case for recursion
+    if len(x) == 1 and len(y) == 1:
+        return int(x) * int(y)
+    if len(x) < len(y):
+        x = zeroInsert(x, len(y) - len(x))
+    elif len(y) < len(x):
+        y = zeroInsert(y, len(x) - len(y))
+    n = len(x)
+    j = n//2
+    #for odd digit integers
+    if (n % 2) != 0:
+        j += 1    
+    BZeroPadding = n - j
+    AZeroPadding = BZeroPadding * 2
+    a = int(x[:j])
+    b = int(x[j:])
+    c = int(y[:j])
+    d = int(y[j:])
+    #recursively calculate
+    ac = karatsubaMultiplication(a, c)
+    bd = karatsubaMultiplication(b, d)
+    k = karatsubaMultiplication(a + b, c + d)
+    A = int(zeroInsert(str(ac), AZeroPadding, False))
+    B = int(zeroInsert(str(k - ac - bd), BZeroPadding, False))
+    return A + B + bd
+
+usIN = list(map(int, input().split()))
+
+print(karatsubaMultiplication(usIN[0] , usIN[1]))
